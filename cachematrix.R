0939dbfe855fcd2b7edb318820ebf54f79796327
Gangani Geethika Wijewardena <geethika01@gmail.com>
2015-01-24 23:35:30 +1100

    cache inverse of matrix
    functions caches the inverse of a given matrix to avoid recomputation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,28 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## Create matrix object, and returns a list of functions to get the matrix, 
+## get the inverse of the matrix,
+## and set the inverse of the matrix. 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #create the matrix
+  mtx <- matrix(vector,numrow, numcol)
+  
+  # assign matrix inverse value to NULL
+  mtxInv <- NULL
+  
+  # get matrix inverse
+  getmtxInv <- function() mtxInv
+  
+  # get matrix data
+  getData <- function() mtx
+  
+  # set the matrix inverse values
+  setInv <- function(inv) mtxInv <<- inv
+  
+  # create output list
+  outputList <- list(getmatrix = getData, getmtxInvVal = getmtxInv, setInvVal = setInv)
+  
 }
 
 
